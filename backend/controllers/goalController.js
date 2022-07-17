bda557ace809773f020bbb09d8cf92d5de233870
LucasMelvin15 <melvinlucas15@gmail.com>
2022-07-17 10:15:14 +0300

    initial REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,12 @@
-const asyncHandler = require(express-async-handler)
+const asyncHandler = require('express-async-handler')
+
+const Goal = require('../model/goalModel')
 //@description Get goals
 //@route GET/api/goals
 //@access Private
 const getGoals =asyncHandler( async (req , res) =>{
-    res.status(200).json({message : 'Get Goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 //@description Set goals
 //@route POST/api/goals
@@ -13,21 +16,40 @@ const setGoal =asyncHandler( async (req , res) =>{
         res.status(400)
         throw new Error('Please add a text field')
     }
+    const goal = await Goal.create({
+        text : req.body.text
+    })
 
-    res.status(200).json({message : 'Set Goals'})
+    res.status(200).json(goal)
 })
 //@description Update goals
 //@route PUT/api/goals/:id
 //@access Private
 const updateGoal =asyncHandler( async (req , res) =>{
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not Found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{
+        new:true,
+    })
     
-    res.status(200).json({message : `Update goal ${req.params.id}`})
+    res.status(200).json(updatedGoal)
 })
 //@description Delete goals
 //@route DELETE/api/goals/:id
 //@access Private
 const deleteGoal =asyncHandler( async (req , res) =>{
-    res.status(200).json({message : `Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not Found')
+    }
+      await goal.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
